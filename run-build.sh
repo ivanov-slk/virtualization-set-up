ac46daa9d475fa144a1204703bc3ef8990bed2a3
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-04 14:15:58 +0200

    reverted to user/pass in packer

--- run-build.sh
+++ run-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -e
 
-rm -r /home/slav/VirtualBox\ VMs/packer-ubuntu-20.04-amd64/
+rm -r /home/slav/VirtualBox\ VMs/packer-ubuntu-20.04-amd64/ || true  # remove if exists
 packer hcl2_upgrade ubuntu2004.json
 PACKER_LOG=1 packer build ubuntu2004.json.pkr.hcl
 vagrant up
