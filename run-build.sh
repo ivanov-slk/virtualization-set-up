b07db2bfca2857614b9716fd525dbb46579657b7
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-05 07:04:32 +0200

    remove all temporary files

--- run-build.sh
+++ run-build.sh
@@ -2,6 +2,9 @@
 set -e
 
 rm -r /home/slav/VirtualBox\ VMs/packer-ubuntu-20.04-amd64/ || true  # remove if exists
+rm -r .vagrant || true
+rm -r *.box || true
+
 packer hcl2_upgrade ubuntu2004.json
 PACKER_LOG=1 packer build ubuntu2004.json.pkr.hcl
 vagrant up
