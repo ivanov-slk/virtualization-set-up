74edc9a5054d7b526f1997c6d6f6a6949af6afe7
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-05 09:52:34 +0200

    explicitly remove all vagrant boxes

--- run-build.sh
+++ run-build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/bash
-set -e
 
-rm -r /home/slav/VirtualBox\ VMs/packer-ubuntu-20.04-amd64/ || true  # remove if exists
+# remove unneeded files
+rm -r /home/slav/VirtualBox\ VMs/packer-ubuntu-20.04-amd64/
 rm -r .vagrant || true
 rm -r *.box || true
 
+set -e
+# packer
 packer hcl2_upgrade ubuntu2004.json
 PACKER_LOG=1 packer build ubuntu2004.json.pkr.hcl
+
+# vagrant
+vagrant box list | cut -f 1 -d ' ' | xargs -L 1 vagrant box remove -f
 vagrant up
