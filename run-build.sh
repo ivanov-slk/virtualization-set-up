17976c90ba9f3c401d2433d52b33c83af0d4961f
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-05 11:03:13 +0200

    remove all vagrant artifacts

--- run-build.sh
+++ run-build.sh
@@ -5,11 +5,11 @@ rm -r /home/slav/VirtualBox\ VMs/packer-ubuntu-20.04-amd64/
 rm -r .vagrant || true
 rm -r *.box || true
 
-set -e
 # packer
 packer hcl2_upgrade ubuntu2004.json
 PACKER_LOG=1 packer build ubuntu2004.json.pkr.hcl
 
 # vagrant
 vagrant box list | cut -f 1 -d ' ' | xargs -L 1 vagrant box remove -f
+vagrant box list | cut -f 1 -d ' ' | xargs -L 1 vagrant destroy -f
 vagrant up
