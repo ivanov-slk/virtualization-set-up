76f41529c036bb0e4c6ba886f28578c3aac2fe74
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-15 17:00:49 +0200

    removed variable defaults

--- vbox-cluster/variables.tf
+++ vbox-cluster/variables.tf
@@ -1,19 +1,19 @@
 variable "virtual_machine_image_master" {
     type = string
-    default = "ubuntu-20.04.3-live-server-amd64"
+    nullable = false 
 }
 
 variable "private_key_path_master" {
   type    = string
-  default = "/home/slav/.ssh/virtual_id_ed25519"
+  nullable = false 
 }
 
 variable "virtual_machine_image_worker" {
     type = string
-    default = "ubuntu-20.04.3-live-server-amd64"
+    nullable = false 
 }
 
 variable "private_key_path_worker" {
   type    = string
-  default = "/home/slav/.ssh/virtual_id_ed25519"
+  nullable = false 
 }
