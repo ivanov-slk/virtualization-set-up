cfa504071e78c137d026f1e3a88fe404e9b713f1
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-15 17:01:10 +0200

    added variables to the module

--- main.tf
+++ main.tf
@@ -9,4 +9,8 @@ terraform {
 
 module "vbox-cluster" {  
   source = "./vbox-cluster"
+  virtual_machine_image_master = var.virtual_machine_image_master
+  private_key_path_master = var.private_key_path_master
+  virtual_machine_image_worker = var.virtual_machine_image_worker
+  private_key_path_worker = var.private_key_path_worker
 }
