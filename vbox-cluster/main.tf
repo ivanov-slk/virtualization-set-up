02cab8a431d8990ce0e9346a4b75742198aba7e5
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-17 09:39:17 +0200

    force vbox-cluster update if vagrantfile changes

--- vbox-cluster/main.tf
+++ vbox-cluster/main.tf
@@ -9,6 +9,9 @@ terraform {
 resource "vagrant_vm" "ubuntu-server-master" {
   vagrantfile_dir = "./vbox-cluster"
   env = {
+    # force terraform to re-run vagrant if the Vagrantfile changes
+    VAGRANTFILE_HASH = md5(file("./Vagrantfile")),
+    
     virtual_machine_image_master = var.virtual_machine_image_master,
     private_key_path_master = var.private_key_path_master,
     virtual_machine_image_worker = var.virtual_machine_image_worker,
