cc9e141e90863f0e1a6796a4a5764139e01391e7
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-19 14:01:25 +0200

    fixed the outputs of vbox-cluster

--- vbox-cluster/outputs.tf
+++ vbox-cluster/outputs.tf
@@ -1,9 +1,9 @@
 output "virtual_machine_names" {
-    description = "The names of the Vagrant-created VirtualBox machines."
-    value       = vagrant_vm.ubuntu-server-instance[*].machine_names
+    description = "The exposed ports of the Vagrant-created VirtualBox machines."
+    value       = vagrant_vm.ubuntu-server-instance[*].machine_names[0]
 }
 
 output "virtual_machine_ports" {
     description = "The exposed ports of the Vagrant-created VirtualBox machines."
-    value       = vagrant_vm.ubuntu-server-instance[*].ports
+    value       = vagrant_vm.ubuntu-server-instance[*].ports[0][0].host
 }
