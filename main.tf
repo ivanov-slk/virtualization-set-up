1e696d3dca93a92bd55213f73ab9538b494099df
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-19 14:01:12 +0200

    fixed the inputs to kubernetes-cluster

--- main.tf
+++ main.tf
@@ -10,15 +10,20 @@ terraform {
 module "packer-vmis" {
   source = "./packer-vmis"
   virtual_machine_image = var.virtual_machine_image
-  private_key_path = var.private_key_path
-
-  
+  private_key_path = var.private_key_path  
 }
 
 module "vbox-cluster" {  
   source = "./vbox-cluster"
   virtual_machine_image = var.virtual_machine_image
   private_key_path = var.private_key_path
+}
+
+module "kubernetes-cluster" {  
+  source = "./kubernetes-cluster"
+  private_key_path = var.private_key_path
+  virtual_machine_ports = module.vbox-cluster.virtual_machine_ports
+  virtual_machine_names = module.vbox-cluster.virtual_machine_names
 }
 
 
