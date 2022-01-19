93d57ff6c42297a100e40db5e274c4d8920ba025
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-19 20:35:41 +0200

    virtual-machine-instance to output map instead of two lists'

--- virtual-machine-instance/outputs.tf
+++ virtual-machine-instance/outputs.tf
@@ -1,9 +1,4 @@
-output "virtual_machine_names" {
+output "virtual_machine_configuration" {
     description = "The exposed ports of the Vagrant-created VirtualBox machines."
-    value       = vagrant_vm.ubuntu-server-instance[*].machine_names[0]
-}
-
-output "virtual_machine_ports" {
-    description = "The exposed ports of the Vagrant-created VirtualBox machines."
-    value       = vagrant_vm.ubuntu-server-instance[*].ports[0][0].host
+    value       = zipmap(vagrant_vm.ubuntu-server-instance[*].machine_names[0], vagrant_vm.ubuntu-server-instance[*].ports[0][0].host)
 }
