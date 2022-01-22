cfae34c4d2a658611b024eb97e4dacd1d8f35230
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-22 16:04:53 +0200

    updated the inputs to kubernetes-cluster module

--- main.tf
+++ main.tf
@@ -10,19 +10,23 @@ terraform {
 module "packer-vmis" {
   source = "./packer-vmis"
   virtual_machine_image = var.virtual_machine_image
-  private_key_path = var.private_key_path  
+  private_key_path = var.private_key_path 
 }
 
 module "virtual-machine-instance" {  
   source = "./virtual-machine-instance"
   virtual_machine_image = var.virtual_machine_image
   private_key_path = var.private_key_path
+
+  depends_on = [module.packer-vmis]
 }
 
 module "kubernetes-cluster" {  
   source = "./kubernetes-cluster"
   private_key_path = var.private_key_path
   virtual_machine_configuration = module.virtual-machine-instance.virtual_machine_configuration
+  virtual_machine_ports = module.virtual-machine-instance.virtual_machine_ports
+  virtual_machine_names = module.virtual-machine-instance.virtual_machine_names
 
   depends_on = [module.virtual-machine-instance]
 }
