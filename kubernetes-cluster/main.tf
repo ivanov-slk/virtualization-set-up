2a35033aac213348a11b3cb3a1cbe09ff771216b
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-24 21:04:36 +0200

    added ansible configuration

--- kubernetes-cluster/main.tf
+++ kubernetes-cluster/main.tf
@@ -21,7 +21,11 @@ connection {
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -u vagrant -i '127.0.0.1:${var.virtual_machine_ports[count.index]},' --private-key ${var.private_key_path} ${var.virtual_machine_ports[count.index] == 2222 ? "./kubernetes-cluster/ansible-playbooks/master-playbook.yml" : "./kubernetes-cluster/ansible-playbooks/node-playbook.yml"}"
+    command = "ansible-playbook -u vagrant 
+               -i '127.0.0.1:${var.virtual_machine_ports[count.index]},' 
+               --private-key ${var.private_key_path} 
+                ${var.virtual_machine_ports[count.index] == 2222 ? 
+                    "./kubernetes-cluster/ansible-playbooks/master-playbook.yml" : "./kubernetes-cluster/ansible-playbooks/node-playbook.yml"}
+                -e k8s_node_public_ip=192.168.50.${count.index + 10}"
   }
-
 }
