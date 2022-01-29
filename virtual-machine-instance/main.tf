b7b9b8573681d56181e1a8a3b15e6f3811d71cc7
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-29 20:15:03 +0200

    a total mess, but keep it for the checkpoint and refactor

--- virtual-machine-instance/main.tf
+++ virtual-machine-instance/main.tf
@@ -14,7 +14,7 @@ resource "vagrant_vm" "virtual-machine-instance" {
     private_key_path = var.private_key_path,
     virtual_machine_image = var.virtual_machine_image,
     virtual_machine_index = count.index,
-    virtual_machine_hostname = count.index < 2 ? "master" : "worker",
+    #virtual_machine_hostname = count.index < 2 ? "master" : "worker",
     ip_base = var.ip_base
   }
   get_ports = true
