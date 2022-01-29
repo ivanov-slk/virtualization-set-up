090402a107c44f6e423035489ea228c2fe1dace2
Slav Ivanov <ivanov.slk@abv.bg>
2022-01-29 15:59:32 +0200

    update environment variables passing

--- virtual-machine-instance/main.tf
+++ virtual-machine-instance/main.tf
@@ -11,9 +11,11 @@ resource "vagrant_vm" "virtual-machine-instance" {
   vagrantfile_dir = "./virtual-machine-instance"
   env = {
 
-    virtual_machine_image = var.virtual_machine_image,
     private_key_path = var.private_key_path,
-    virtual_machine_index = count.index
+    virtual_machine_image = var.virtual_machine_image,
+    virtual_machine_index = count.index,
+    virtual_machine_hostname = count.index < 2 ? "master" : "worker",
+    ip_base = var.ip_base
   }
   get_ports = true
 }
