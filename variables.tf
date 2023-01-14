e357a1ec9fa000b908995049a798d658b0736060
Slav Ivanov <ivanov.slk@abv.bg>
2023-01-14 11:19:47 +0200

    Update to new Ubuntu image.

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "virtual_machine_image" {
   type    = string
-  default = "ubuntu-20.04.3-live-server-amd64"
+  default = "ubuntu-22.10-live-server-amd64"
 }
 
 variable "private_key_path" {
