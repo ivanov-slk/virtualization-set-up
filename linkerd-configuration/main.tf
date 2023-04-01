923d61d2b9ff7627f2beae132376b9c330a4e269
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-01 13:05:59 +0300

    Move restart of prometheus pods into linkerd configuration. This is error-prone.

--- linkerd-configuration/main.tf
+++ linkerd-configuration/main.tf
@@ -118,3 +118,17 @@ resource "null_resource" "web_deployment_patch" {
 
   depends_on = [helm_release.linkerd_viz]
 }
+
+# TODO Refactor, this depends on resources in other modules and is error-prone.
+# Restart all pods to have linkerd start tracking them.
+resource "null_resource" "restart_prometheus_stack" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOT
+      "kubectl rollout restart deployment kube-prometheus-stack-grafana -n prometheus"
+      "kubectl rollout restart deployment kube-prometheus-stack-kube-state-metrics -n prometheus"
+      "kubectl rollout restart deployment kube-prometheus-stack-operator -n prometheus"
+      "kubectl rollout restart ds kube-prometheus-stack-prometheus-node-exporter -n prometheus"
+    EOT
+  }
+}
