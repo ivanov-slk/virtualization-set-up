3f33710b3e19dca7bd7ff40ae8792bda856f2d49
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-01 07:46:10 +0300

    Reorder creation of additional scrape configuration.

--- prometheus-stack/main.tf
+++ prometheus-stack/main.tf
@@ -11,6 +11,24 @@ resource "kubectl_manifest" "namespace_prometheus" {
   yaml_body = file("./prometheus-stack/namespace-prometheus.yaml")
 }
 
+resource "kubectl_manifest" "additional_scrape_configs_secret" {
+  yaml_body = file("./prometheus-stack/prometheus-additional-scrape-configs-secret.yaml")
+
+  depends_on = [
+    helm_release.namespace_prometheus
+  ]
+}
+
+# Needs to be executed only once, and does not need to be managed by Terraform, therefore the local exec.
+# TODO Consider using something less ad hoc. 
+resource "null_resource" "add_custom_linkerd_jobs_patch" {
+  provisioner "local-exec" {
+    command = "kubectl patch prometheus kube-prometheus-stack-prometheus -n prometheus --type merge --patch-file prometheus-stack/prometheus-additional-scrape-configs-patch.yaml"
+  }
+
+  depends_on = [helm_release.namespace_prometheus]
+}
+
 resource "helm_release" "kube_prometheus_stack" {
   name             = "kube-prometheus-stack"
   namespace        = "prometheus"
@@ -31,22 +49,4 @@ resource "kubectl_manifest" "service_lb_grafana" {
   depends_on = [
     helm_release.kube_prometheus_stack
   ]
-}
-
-resource "kubectl_manifest" "additional_scrape_configs_secret" {
-  yaml_body = file("./prometheus-stack/prometheus-additional-scrape-configs-secret.yaml")
-
-  depends_on = [
-    helm_release.kube_prometheus_stack
-  ]
-}
-
-# Needs to be executed only once, and does not need to be managed by Terraform, therefore the local exec.
-# TODO Consider using something less ad hoc. 
-resource "null_resource" "add_custom_linkerd_jobs_patch" {
-  provisioner "local-exec" {
-    command = "kubectl patch prometheus kube-prometheus-stack-prometheus -n prometheus --type merge --patch-file prometheus-stack/prometheus-additional-scrape-configs-patch.yaml"
-  }
-
-  depends_on = [helm_release.kube_prometheus_stack]
 }
