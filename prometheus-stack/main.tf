48424db3552064ec64eafe82781919d78a055168
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-13 14:35:22 +0300

    Refactor.

--- prometheus-stack/main.tf
+++ prometheus-stack/main.tf
@@ -33,6 +33,42 @@ resource "helm_release" "kube_prometheus_stack" {
   depends_on = [kubectl_manifest.namespace_prometheus]
 }
 
+resource "kubectl_manifest" "pod_monitor_strimzi_cluster_operator" {
+  yaml_body = file("./prometheus-stack/prometheus-pod-monitor-strimzi-cluster-operator.yaml")
+
+  depends_on = [
+    helm_release.kube_prometheus_stack
+  ]
+}
+resource "kubectl_manifest" "pod_monitor_strimzi_kafka_bridge" {
+  yaml_body = file("./prometheus-stack/prometheus-pod-monitor-strimzi-kafka-bridge.yaml")
+
+  depends_on = [
+    helm_release.kube_prometheus_stack
+  ]
+}
+resource "kubectl_manifest" "pod_monitor_strimzi_kafka_entity_operator" {
+  yaml_body = file("./prometheus-stack/prometheus-pod-monitor-strimzi-kafka-entity-operator.yaml")
+
+  depends_on = [
+    helm_release.kube_prometheus_stack
+  ]
+}
+resource "kubectl_manifest" "pod_monitor_strimzi_kafka_resources" {
+  yaml_body = file("./prometheus-stack/prometheus-pod-monitor-strimzi-kafka-resources.yaml")
+
+  depends_on = [
+    helm_release.kube_prometheus_stack
+  ]
+}
+resource "kubectl_manifest" "prometheus_rules_strimzi" {
+  yaml_body = file("./prometheus-stack/prometheus-rules-strimzi.yaml")
+
+  depends_on = [
+    helm_release.kube_prometheus_stack
+  ]
+}
+
 resource "kubectl_manifest" "service_lb_grafana" {
   yaml_body = file("./prometheus-stack/grafana-service-lb.yaml")
 
@@ -43,7 +79,7 @@ resource "kubectl_manifest" "service_lb_grafana" {
 
 # Needs to be executed only once, and does not need to be managed by Terraform, therefore the local exec.
 # TODO Consider using something less ad hoc. 
-resource "null_resource" "add_custom_linkerd_jobs_patch" {
+resource "null_resource" "add_custom_jobs_patch" {
   provisioner "local-exec" {
     command = "kubectl patch prometheus kube-prometheus-stack-prometheus -n prometheus --type merge --patch-file prometheus-stack/prometheus-additional-scrape-configs-patch.yaml"
   }
