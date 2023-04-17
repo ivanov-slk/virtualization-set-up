1458a2611c4afa046aace72076d908baf85032ca
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-17 18:46:38 +0300

    Tidy outputs.

--- argocd/outputs.tf
+++ argocd/outputs.tf
@@ -1,4 +1,4 @@
 output "argocd-metadata" {
   description = "The details of the manifests used to deploy ArgoCD."
-  value       = kubectl_manifest.argocd_install_manifests[*].kind
+  value       = kubectl_manifest.argocd_install_manifests[*]
 }
--- outputs.tf
+++ outputs.tf
@@ -3,14 +3,3 @@ output "virtual-machine-instance-outputs" {
   value       = module.kubernetes-cluster
 }
 
-output "prometheus-metadata" {
-  description = "The details of the Helm chart used to deploy the Prometheus stack."
-  value       = module.prometheus-stack
-  sensitive   = true
-}
-
-output "linkerd-dashboard-metadata" {
-  description = "The details of the Helm chart used to deploy the dashboard of Linkerd."
-  value       = module.linkerd
-  sensitive   = true
-}
