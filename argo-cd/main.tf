8198409625f70f273feccc372730c3f65d97e7cd
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-17 18:27:25 +0300

    Initial configuration for Argo CD.

---
+++ argo-cd/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
+
+resource "kubectl_manifest" "namespace_argocd" {
+  yaml_body = file("./argocd/namespace-argocd.yaml")
+}
+
+data "http" "argocd_yaml_raw" {
+  url = "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
+}
+
+data "kubectl_file_documents" "argocd_install_manifests" {
+  content = data.http.argocd_yaml_raw
+}
+
+resource "kubectl_manifest" "argocd_install_manifests" {
+  for_each  = data.kubectl_file_documents.argocd_install_manifests.manifests
+  yaml_body = each.value
+}
+
+resource "kubectl_manifest" "argocd_lb" {
+  yaml_body = file("./argocd/argocd-service-lb.yaml")
+
+  depends_on = [kubectl_manifest.argocd_install_manifests]
+}
+
+
