0f6f642499be613d567ad23e582b196169f2781d
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-20 18:12:51 +0300

    Remove strimzi configs from prometheus.

--- prometheus-stack/main.tf
+++ prometheus-stack/main.tf
@@ -19,18 +19,18 @@ resource "kubectl_manifest" "additional_scrape_configs_secret" {
   ]
 }
 
-data "kubectl_filename_list" "grafana_strimzi_dashboards" {
-  pattern = "./prometheus-stack/grafana-strimzi-dashboards/*.yaml"
-}
+# data "kubectl_filename_list" "grafana_strimzi_dashboards" {
+#   pattern = "./prometheus-stack/grafana-strimzi-dashboards/*.yaml"
+# }
 
-resource "kubectl_manifest" "grafana_strimzi_dashboards" {
-  count     = length(data.kubectl_filename_list.grafana_strimzi_dashboards.matches)
-  yaml_body = file(element(data.kubectl_filename_list.grafana_strimzi_dashboards.matches, count.index))
+# resource "kubectl_manifest" "grafana_strimzi_dashboards" {
+#   count     = length(data.kubectl_filename_list.grafana_strimzi_dashboards.matches)
+#   yaml_body = file(element(data.kubectl_filename_list.grafana_strimzi_dashboards.matches, count.index))
 
-  depends_on = [
-    kubectl_manifest.namespace_prometheus
-  ]
-}
+#   depends_on = [
+#     kubectl_manifest.namespace_prometheus
+#   ]
+# }
 
 resource "helm_release" "kube_prometheus_stack" {
   name             = "kube-prometheus-stack"
@@ -46,18 +46,18 @@ resource "helm_release" "kube_prometheus_stack" {
   depends_on = [kubectl_manifest.namespace_prometheus]
 }
 
-data "kubectl_filename_list" "prometheus_strimzi_additional_configuration" {
-  pattern = "./prometheus-stack/prometheus-strimzi/*.yaml"
-}
+# data "kubectl_filename_list" "prometheus_strimzi_additional_configuration" {
+#   pattern = "./prometheus-stack/prometheus-strimzi/*.yaml"
+# }
 
-resource "kubectl_manifest" "prometheus_strimzi_additional_configuration" {
-  count     = length(data.kubectl_filename_list.prometheus_strimzi_additional_configuration.matches)
-  yaml_body = file(element(data.kubectl_filename_list.prometheus_strimzi_additional_configuration.matches, count.index))
+# resource "kubectl_manifest" "prometheus_strimzi_additional_configuration" {
+#   count     = length(data.kubectl_filename_list.prometheus_strimzi_additional_configuration.matches)
+#   yaml_body = file(element(data.kubectl_filename_list.prometheus_strimzi_additional_configuration.matches, count.index))
 
-  depends_on = [
-    helm_release.kube_prometheus_stack
-  ]
-}
+#   depends_on = [
+#     helm_release.kube_prometheus_stack
+#   ]
+# }
 
 resource "kubectl_manifest" "service_lb_grafana" {
   yaml_body = file("./prometheus-stack/grafana-service-lb.yaml")
