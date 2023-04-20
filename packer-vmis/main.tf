cc2568dd1be1d9005e490532f904f5debb2c67af
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-20 18:09:03 +0300

    Tidy up packer variables.

--- packer-vmis/main.tf
+++ packer-vmis/main.tf
@@ -2,8 +2,10 @@
 
 resource "null_resource" "packer-build" {
   triggers = {
-    virtual_machine_image = var.virtual_machine_image
-    always_run            = "${timestamp()}"
+    distribution_name            = var.distribution_name
+    virtual_machine_image        = var.virtual_machine_image
+    virtual_machine_image_exists = fileexists(".packer-vmis/${var.virtual_machine_image}.box")
+    # always_run            = "${timestamp()}"
   }
   provisioner "local-exec" {
     working_dir = "."
@@ -14,7 +16,7 @@ BLUE='\033[0;34m' # Blue Text
 NC='\033[0m' # No Color
 
 cd packer-vmis
-packer build ubuntu-22.10-live-server-amd64.pkr.hcl
+packer build ubuntu-${self.triggers.virtual_machine_image}.pkr.hcl -var "distribution_name=${self.triggers.distribution_name}"
 
 if [ $? -eq 0 ]; then
   printf "\n $GREEN Packer Succeeded $NC \n"
@@ -25,9 +27,9 @@ fi
 EOF
   }
 
-  # # Keep the image to avoid rebuilding each time; uncomment when stable.
-  # provisioner "local-exec" {
-  #   when    = destroy
-  #   command = "rm -rf ./packer-vmis/${var.virtual_machine_image}.box"
-  # }
+  # Keep the image to avoid rebuilding each time; uncomment when stable.
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -rf ./packer-vmis/${self.triggers.virtual_machine_image}.box"
+  }
 }
--- packer-vmis/variables.tf
+++ packer-vmis/variables.tf
@@ -1,9 +1,14 @@
+variable "distribution_name" {
+  type     = string
+  nullable = false
+}
+
 variable "virtual_machine_image" {
-    type = string
-    nullable = false 
+  type     = string
+  nullable = false
 }
 
 variable "private_key_path" {
-  type    = string
-  nullable = false 
+  type     = string
+  nullable = false
 }
